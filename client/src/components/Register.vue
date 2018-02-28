740200a1e280c1b63087f1155a1955a0eaa595b7
Staffan Palopää <staffanpalopaa@Staffans-MacBook-Pro.local>
2018-02-28 17:12:25 +0100

    user registration and validation with styling

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,23 +1,34 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <input
-      type="email"
-      name="email"
-      v-model="email"
-      placeholder="email" />
-    <br>
-    <input
-      type="password"
-      name="password"
-      v-model="password"
-      placeholder="password">
-    <br>
-    <button
-      @click="register">
-      Register
-    </button>
-  </div>
+  <v-layout column="">
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="blue lighten-3">
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <input
+            type="email"
+            name="email"
+            v-model="email"
+            placeholder="email" />
+          <br>
+          <input
+            type="password"
+            name="password"
+            v-model="password"
+            placeholder="password">
+          <br>
+          <div class="error" v-html="error" />
+          <br>
+          <v-btn
+            class="blue lighten-3"
+            @click="register">
+            Register
+          </v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -26,17 +37,20 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
-      console.log('register button was clicked', this.email, this.password)
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
@@ -44,4 +58,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.error {
+  color: red;
+}
 </style>
